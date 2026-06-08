10637144af0140b07db24eb7a5849e1e89aab63e
Ilnnn <camoreze@gmail.com>
2026-06-08 15:12:36 +0200

    adding all files to get a proper version of the remote branch

---
+++ src/logoMap.js
@@ -0,0 +1,10 @@
+import fnatic from './assets/fnatic.png';
+import giantx from './assets/giantx.png';
+import sk from './assets/sk.png';
+// ... importe tous tes logos ici
+
+export const logos = {
+  FNATIC: fnatic,
+  GiantX: giantx,
+  "SK Gaming": sk,
+};
